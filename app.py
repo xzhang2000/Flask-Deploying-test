57b2ac6c1249011919c15e39824a4b22d378f98f
xzhang2000 <xzhang2000@outlook.com>
2026-03-17 12:04:06 +0800

    202603171204

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import sqlite3
 from flask import Flask, render_template, request
+from flask_frozen import Freezer
 
 app = Flask(__name__)
+freezer = Freezer(app)  # 初始化Freezer
+
 
 
 @app.route("/")
@@ -129,4 +132,4 @@ def team():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    freezer.freeze()
